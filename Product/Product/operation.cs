cf5cf15ec5e3d69465b52d91fbcc70810891f520
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-31 10:33:09 +0530

    [Prasanna] Add:Implement the Linq concept in the Product Management

--- Product/Product/operation.cs
+++ Product/Product/operation.cs
@@ -74,14 +74,8 @@ namespace ProductReviewManagement
         }
         public void RetreiveisLikeMethodisTrue(List<product> list)
         {
-            /*var result = list.Where(x => x.isLike == true);
-            Display(result.ToList());*/
-            var result = table.AsEnumerable().Where(x => x.Field<bool>("IsLike").Equals(true));
-            foreach (var item in result.AsEnumerable())
-            {
-                Console.WriteLine(item.Field<int>("ProductId") + " " + item.Field<int>("UserID") + " " + item.Field<int>("Rating") + " " + item.Field<string>("Review") + " " + item.Field<bool>("isLike"));
-            }
-
+            var result = list.Where(x => x.isLike == true);
+            Display(result.ToList());
         }
         public void RetreiveRatingisAvg(List<product> list)
         {
